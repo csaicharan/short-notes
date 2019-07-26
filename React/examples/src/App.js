1361b35b3c7fda495f116cd327fa3f6dd01bcdcb
Sai Ch <sai.charanch@thermofisher.com>
2019-07-26 20:14:50 +0530

    React functional Component Example

--- React/examples/src/App.js
+++ React/examples/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import React from 'react';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       Initial Commit
+      <Person />
     </div>
   );
 }
---
+++ React/examples/src/Person/Person.js
@@ -0,0 +1,9 @@
+// While creating the functional component we must keep in mind we should import React to use JSX syntax in the js files
+
+import React from 'react';
+
+const Person = () => {
+    return <p1>I'm a person.</p1>;
+}
+
+export default Person;
