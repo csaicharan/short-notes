f916095d6bb394beda39d1b7a30ed093eac07e3a
Sai Ch <sai.charanch@thermofisher.com>
2019-08-21 21:51:50 +0530

    Loding components from route and fixing reloading of the page

--- React/routing-example/src/containers/Layout/Layout.js
+++ React/routing-example/src/containers/Layout/Layout.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 /**
  * Step 2: Should import Route to start using routing in the application
  */
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import Home from '../../components/Home/Home';
 
 import CSSClasses from './Layout.module.css';
@@ -18,13 +18,14 @@ class Layout extends Component{
                     <nav className={CSSClasses.nav}>
                         <ol>
                             <li>
-                                <a href="/">Home</a>
+                                {/* If we use <a/> tag it reloads the application when we click the link, to use react render to navigate between the pages use Link component from react-renders-dom */}
+                                <Link to="/">Home</Link>
                             </li>
                             <li>
-                                <a href="/parent-one">Parent One</a>
+                                <Link to="/parent-one">Parent One</Link>
                             </li>
                             <li>
-                                <a href="/parent-two">Parent Two</a>
+                                <Link to="/parent-two">Parent Two</Link>
                             </li>
                         </ol>
                     </nav>
@@ -33,8 +34,9 @@ class Layout extends Component{
                     {/* Here Exact refers to check the exact URL of the route if you wont use it will takes as a base path and renders in all the paths starting with the same path */}
                     <Route path="/" exact render={() => <Home>Home 1</Home>}/>
                     <Route path="/" render={() => <Home>Home 2</Home>}/>
-                    <Route path="/parent-one" render={() => <ParentOne/>}/>
-                    <Route path="/parent-two" render={() => <ParentTwo/>}/>
+                    {/* Binding component to the route with the following code */}
+                    <Route path="/parent-one" component={ParentOne}/>
+                    <Route path="/parent-two" component={ParentTwo}/>
                 </main>
             </div>
         )
